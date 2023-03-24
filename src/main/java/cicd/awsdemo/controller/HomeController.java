dcc81649f91ac78b3c01b5066404fcc0dc2f26f7
Hemant <hjangid125@gmail.com>
2023-03-24 22:51:29 +0530

    ffgvrf

--- src/main/java/cicd/awsdemo/controller/HomeController.java
+++ src/main/java/cicd/awsdemo/controller/HomeController.java
@@ -9,6 +9,6 @@ public class HomeController {
 
     @RequestMapping
     public String getDemo(){
-        return "welcome aws testing !!";
+        return "welcome aws testing Hemant and Rahul did it !!";
     }
 }
