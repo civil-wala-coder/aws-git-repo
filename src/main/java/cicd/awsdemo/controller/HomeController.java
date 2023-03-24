16c8e6b5398d525857b55a747d0015c23cffa7da
Hemant Jangid <127328525+hemant-jangid-24@users.noreply.github.com>
2023-03-24 22:28:36 +0530

    Update HomeController.java

--- src/main/java/cicd/awsdemo/controller/HomeController.java
+++ src/main/java/cicd/awsdemo/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-    @RequestMapping("/api")
+    @RequestMapping
     public String getDemo(){
         return "welcome aws !!";
     }
