5cd75c89dbd0c9e6d50a73c3c21c18c9d03e7f49
Hemant <hjangid125@gmail.com>
2023-03-24 19:46:41 +0530

    first commit

---
+++ src/main/java/cicd/awsdemo/AwsDemoApplication.java
@@ -0,0 +1,14 @@
+package cicd.awsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsDemoApplication.class, args);
+		System.out.println("done-------------");
+	}
+
+}
---
+++ src/main/java/cicd/awsdemo/controller/HomeController.java
@@ -0,0 +1,14 @@
+package cicd.awsdemo.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/api")
+    public String getDemo(){
+        return "welcome aws !!";
+    }
+}
